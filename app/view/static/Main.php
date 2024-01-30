cc71c77805cf7538bf01f4427326942f875dd564
junaedimg <jmg290601@gmail.com>
2024-01-30 23:41:24 +0700

    Improve UI navigation

--- app/view/static/Main.php
+++ app/view/static/Main.php
@@ -4,36 +4,82 @@ use app\config\config;
 ?>
 
 <!DOCTYPE html>
-<html lang="en" data-bs-theme=dark>
+<html lang="en" data-bs-theme="light">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>STORE APP</title>
     <link rel="stylesheet" href="<?= config::BASEURL ?>store-app/public/assets/css/custom bootstrap.css">
+    <script src="https://kit.fontawesome.com/1b3a0a4af2.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="container-fluid vh-100 p-0" id="main-page">
+    <div class="container-fluid vh-100 p-0 " id="main-page">
         <div class="d-flex h-100">
             <div class="container w-auto p-0">
                 <!-- NAVIGATION -->
-                <nav class="h-100 p-2">
-                    <h1 class="">Nav</h1>
-                    <ul>
-                        <li><a href="" class="text-nowrap">Dashboard</a></li>
-                        <li><a href="" class="text-nowrap">Purchase</a></li>
-                        <li><a href="" class="text-nowrap">Transaction Log</a></li>
-                        <li><a href="" class="text-nowrap">Manage Inventory</a></li>
-                        <li><a href="" class="text-nowrap">Chart</a></li>
+                <nav class="h-100 p-3">
+                    <div class="container mb-4">
+                        <span>
+                            <h1 class="text-nowrap">App Store</h1>
+                        </span>
+                    </div>
+                    <ul class="link p-0">
+                        <span class=""></span>
+                        <li class="p-2 m-2 list-unstyled rounded-2 active">
+                            <a href="javascript:void(0)" class="text-nowrap">
+                                <div class="logo"><i class="fa-solid fa-chart-line"></i></div>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="p-2 m-2 list-unstyled rounded-2">
+                            <a href="javascript:void(0)" class="text-nowrap">
+                                <div class="logo"><i class="fa-solid fa-cart-shopping"></i></div>
+                                <span>Purchase</span>
+                            </a>
+                        </li>
+                        <li class="p-2 m-2 list-unstyled rounded-2">
+                            <a href="javascript:void(0)" class="text-nowrap">
+                                <div class="logo"><i class="fa-solid fa-book"></i></div> 
+                                <span>Transaction Log</span>
+                            </a>
+                        </li>
+                        <li class="p-2 m-2 list-unstyled rounded-2">
+                            <a href="javascript:void(0)" class="text-nowrap">
+                                <div class="logo"><i class="fa-solid fa-boxes-stacked"></i></div>
+                                <span>Manage Inventory</span>
+                            </a>
+                        </li>
+                        <li class="p-2 m-2 list-unstyled rounded-2">
+                            <a href="javascript:void(0)" class="text-nowrap">
+                                <div class="logo"><i class="fa-solid fa-chart-pie"></i></div>
+                                <span>Chart</span>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
             <div class="container-fluid p-0 d-flex flex-column">
                 <!-- HEADER -->
                 <header class="py-2 px-3 d-flex justify-content-between">
-                    <h1 class="m-0">Header</h1>
-                    <button class="btn btn-primary btn-custom shadow " id="btnSwitch">Toggle Mode</button>
+                    <h1 class="m-0 ">Header</h1>
+                    <div class="option d-flex">
+                        <button class="btn btn-primary btn-custom shadow " id="btnSwitch">Toggle Mode</button>
+                        <div class="account p-2 ms-2 list-unstyled rounded-2">
+                            <div class="user">
+                                <div class="user-info">
+                                    <span class="name">Junaedi M G</span>
+                                    <span class="name">Admin</span>
+                                </div>
+                                <a href="javascript:void(0)" class="text-nowrap logout">
+                                    <i class="fa-solid fa-right-from-bracket"></i>
+                                    <span>Logout</span>
+                                </a>
+                            </div>
+                            <div class="image-user"><img src="" alt=""></div>
+                        </div>
+                    </div>
                 </header>
                 <!-- MAIN -->
                 <main class="flex-grow-1 ms-2 mt-2 p-2 rounded-3">
@@ -53,4 +99,5 @@ use app\config\config;
         }
     })
 </script>
+
 </html>
